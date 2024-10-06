c344be5c1b1368094b39a1ea9c4061a13cc46b15
Haiqin Liu <131931067+liuhaiqinFDU@users.noreply.github.com>
2024-10-06 21:53:32 +0800

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,230 +1,137 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dropdowns = ['eventid', 'window', 
-        'PrimarySector','state'];  //,'SIC4', 'city', 'conml'
-    /*
-    const dropdowns2 = ['eventid2', 
-        'PrimarySector2']; //'SIC42','state2', 'city2', 'conml2'
-    */
+    const dropdowns = ['eventid', 'window', 'PrimarySector', 'state'];
+    let eventTitles = {};
 
-    // Fetch event IDs for the eventid dropdown
-    // fetch('/get_event_ids')
-    fetch('json_data/event_ids.json')
-        .then(response => response.json())
-        .then(event_ids => {
-            console.log("Event IDs received:", event_ids);
-            populateDropdown('eventid', event_ids);
-            //populateDropdown('eventid2', event_ids);
+    // Fetch event IDs and titles for the eventid dropdown
+    fetch('../json_data/event_ids.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json(); // Parse the JSON from the response
+        })
+        .then(data => {
+            // Extract unique eventid values and titles
+            const uniqueEventIds = [...new Set(data.map(item => item.eventid))];
+            eventTitles = data.reduce((acc, item) => {
+                acc[item.eventid] = item.title;
+                return acc;
+            }, {});
+            console.log("Unique Event IDs and Titles received:", uniqueEventIds, eventTitles);
+
+            // Populate the dropdown list
+            populateDropdown('eventid', uniqueEventIds);
 
             // Set default values
-            document.getElementById('eventid').value = 1; //event_ids[0]
-            document.getElementById('window').value = 45;
-            //document.getElementById('eventid2').value = 1;
+            if (uniqueEventIds.length > 0) {
+                document.getElementById('eventid').value = uniqueEventIds[0];
+            }
 
             // Fetch options for the default event ID
             fetchOptions();
-            //fetchOptions2();
         })
-        .catch(error => console.error('Error fetching event IDs:', 
-            error));
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 
     // Event listener for the eventid dropdown
     document.getElementById('eventid').addEventListener('change', () => {
         fetchOptions();
-        fetchData();  // must put it here otw it won't allow you to fetch after changing options
+        fetchData();
     });
-    //document.getElementById('eventid2').addEventListener('change', fetchOptions2);
 
     // Event listener for other dropdowns
     dropdowns.forEach(dropdown => {
-        document.getElementById(dropdown).addEventListener('change', 
-            fetchData);
+        document.getElementById(dropdown).addEventListener('change', fetchData);
     });
-    /*
-    dropdowns2.forEach(dropdown => {
-        document.getElementById(dropdown).addEventListener('change', 
-        fetchData2);
-    });
-    */
 
     function populateDropdown(id, options) {
         const select = document.getElementById(id);
         select.innerHTML = ''; // Clear existing options
-        if (select) {
-            select.innerHTML = ''; // Clear existing options
-            options.forEach(option => {
-                const opt = document.createElement('option');
-                opt.value = option;
-                opt.text = option;
-                select.add(opt);
-            });
-        } else {
-            console.warn(`Dropdown with id ${id} does not exist`);
-        }
+        options.forEach(option => {
+            const opt = document.createElement('option');
+            opt.value = option;
+            opt.text = option;
+            select.add(opt);
+        });
     }
 
     function fetchOptions() {
         const eventid = document.getElementById('eventid').value;
-        /* instead of calling backend calculation in python
-        use pre-calculated figure data
-        fetch('/get_options', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ eventid })
-        })
-        */
-        fetch(`json_data/event${eventid}.json`)
-        .then(response => response.json())
-        .then(data => {
-            const primarySectors = [...new Set(data.map(item => item.PrimarySector))];
-            const states = [...new Set(data.map(item => item.state))];
-            console.log("Options received:", { primarySectors, states });
-            populateDropdown('PrimarySector', primarySectors);
-            populateDropdown('state', states);
+        fetch(`../json_data/event${eventid}.json`)
+            .then(response => response.json())
+            .then(data => {
+                const primarySectors = [...new Set(data.map(item => item.PrimarySector))];
+                const states = [...new Set(data.map(item => item.state))];
+                console.log("Options received:", { primarySectors, states });
+                populateDropdown('PrimarySector', primarySectors);
+                populateDropdown('state', states);
 
-            // Set default values
-            document.getElementById('PrimarySector').value = primarySectors[0];
-            document.getElementById('state').value = states[0];
+                // Set default values
+                document.getElementById('PrimarySector').value = primarySectors[0];
+                document.getElementById('state').value = states[0];
 
-            fetchData(); // Fetch data after setting options
-        })
-        .then(options => {
-            console.log("Options received:", options);
-            populateDropdown('window', options.window);
-            populateDropdown('PrimarySector', options.PrimarySector);
-            //populateDropdown('SIC4', options.SIC4);
-            populateDropdown('state', options.state);
-            //populateDropdown('city', options.city);
-            //populateDropdown('conml', options.conml);
-            
-            // Set a more dynamic default
-            const primarySectorDefault = null; //options.PrimarySector[0] || "Information Technology"; 
-            const stateDefault = null; //options.PrimarySector[0] || "CA"; 
-
-            // Set default value for PrimarySector
-            document.getElementById('PrimarySector').value = primarySectorDefault;
-            //document.getElementById('SIC4').value = null;
-            document.getElementById('state').value = stateDefault;
-            //document.getElementById('city').value = null;
-            //document.getElementById('conml').value = null;
-
-            // Fetch data and plot the chart with default values
-            fetchData();
-        })
-        .catch(error => {
-            console.error('Error fetching event IDs:', error);
-            alert('There was an error fetching the event IDs. Please try again later.');
-        });
+                fetchData(); // Fetch data after setting options
+            })
+            .catch(error => console.error('Error fetching options:', error));
     }
-    /*
-    function fetchOptions2() {
-        const eventid = document.getElementById('eventid2').value;
-        fetch('/get_options', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ eventid })
-        })
-        .then(response => response.json())
-        .then(options => {
-            console.log("Options received:", options);
-            populateDropdown('PrimarySector2', options.PrimarySector);
-            //populateDropdown('conml2', options.conml);
-            //populateDropdown('city2', options.city);
-            //populateDropdown('state2', options.state);
-            //populateDropdown('SIC42', options.SIC4);
 
-            // Set default value for PrimarySector
-            document.getElementById('PrimarySector2').value = "Information Technology";
-            //document.getElementById('SIC4').value = null;
-            //document.getElementById('state').value = null;
-            //document.getElementById('city').value = null;
-            //document.getElementById('conml').value = null;
-
-            // Fetch data and plot the chart with default values
-            fetchData2();
-        })
-        .catch(error => console.error('Error fetching options:', error));
-    }
-    */
     function fetchData() {
-        const data = {
-            eventid: document.getElementById('eventid').value,
-            window: document.getElementById('window').value,
-            PrimarySector: document.getElementById('PrimarySector').value || null,
-            //conml: document.getElementById('conml').value || null,
-            //city: document.getElementById('city').value || null,
-            state: document.getElementById('state').value || null
-            //SIC4: document.getElementById('SIC4').value || null
-        };
+        const eventid = document.getElementById('eventid').value;
+        const window = document.getElementById('window').value;
+        const primarySector = document.getElementById('PrimarySector').value; //||null
+        const state = document.getElementById('state').value; //||null
 
-        // Fetch the data from the API (no, change it to json)
-        /*
-        fetch('/get_data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        */
-        fetch(`json_data/event${eventid}.json`)
-        .then(response => response.json())
-        .then(data => {
-            const filteredData = data.filter(item => 
-                //item.window == window && 
-                (!primarySector || item.PrimarySector === primarySector) &&
-                (!state || item.state === state)
-            );
+        // Fetch the data from the JSON file
+        fetch(`../json_data/event${eventid}.json`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Data fetched for event:", eventid, data);
 
-            if (filteredData.length === 0) {
-                document.getElementById('chart1').innerHTML = 'No data';
-            } else {
-                // Remove "No data" message if it exists
-                if (document.getElementById('chart1').innerHTML === 'No data') {
-                    document.getElementById('chart1').innerHTML = '';
+                const filteredData = data.filter(item => 
+                    (item.PrimarySector === primarySector) &&
+                    (item.state === state)
+                );
+
+                console.log("Filtered data:", filteredData);
+
+                if (filteredData.length === 0) {
+                    document.getElementById('chart1').innerHTML = 'No data';
+                } else {
+                    // Remove "No data" message if it exists
+                    if (document.getElementById('chart1').innerHTML === 'No data') {
+                        document.getElementById('chart1').innerHTML = '';
+                    }
+                    plotData(filteredData, window, 'chart1', eventTitles[eventid]);
                 }
-                plotData(filteredData, window, 'chart1');
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));
+            })
+            .catch(error => console.error('Error fetching data:', error));
     }
 
-    /*
-    function fetchData2() {
-        const data = {
-            eventid: document.getElementById('eventid2').value,
-            PrimarySector: document.getElementById('PrimarySector2').value || null
-            //conml: document.getElementById('conml2').value || null,
-            //city: document.getElementById('city2').value || null,
-            //state: document.getElementById('state2').value || null,
-            //SIC4: document.getElementById('SIC42').value || null,
-        };
+    function plotData(data, window, chartId, title) {
+        console.log("Data received for plotting:", data);
+        console.log("Window:", window);
 
-        // Fetch the data from the API
-        fetch('/get_data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-            plotHistogram(data, 'chart2');
-        })
-        .catch(error => console.error('Error fetching data:', error));
-    */
-    
-    function plotData(data, window, chartId) {
+        // Filter data based on the window parameter
+        let filteredData;
+        if (window == 45) {
+            filteredData = data.filter(item => item.dist >= -15 && item.dist <= 30);
+        } else if (window == 30) {
+            filteredData = data.filter(item => item.dist >= -10 && item.dist <= 20);
+        } else {
+            filteredData = data; // No filtering for other window values
+        }
 
-        const dist = data.map(item => item.dist);
-        const median = data.map(item => item[`cret${window}_median`]);
-        const perc_10 = data.map(item => item[`cret${window}_perc_10`]);
-        const perc_90 = data.map(item => item[`cret${window}_perc_90`]);
-        let title = data[0].title;
+        const dist = filteredData.map(item => item.dist);
+        const median = filteredData.map(item => item[`cret${window}_median`]);
+        const perc_10 = filteredData.map(item => item[`cret${window}_perc_10`]);
+        const perc_90 = filteredData.map(item => item[`cret${window}_perc_90`]);
+
+        console.log("Dist:", dist);
+        console.log("Median:", median);
+        console.log("Perc 10:", perc_10);
+        console.log("Perc 90:", perc_90);
+        console.log("Title:", title);
 
         // Function to insert <br> tags for long titles
         function insertLineBreaks(str, maxLineLength) {
@@ -245,7 +152,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         // Insert line breaks into the title
-        title = insertLineBreaks(title, 100); 
+        title = insertLineBreaks(title, 100);
 
         const traceMedian = {
             x: dist,
@@ -266,51 +173,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const layout = {
             title: title,
-            xaxis: { title: 'Minutes to the Event' },
-            yaxis: { title: 'Cumulative Returns' }
+            xaxis: { title: 'DIST' },
+            yaxis: { title: 'CRET' }
         };
 
         Plotly.newPlot(chartId, [traceBand, traceMedian], layout);
     }
-
-    /*
-    function plotHistogram(data, chartId) {
-        const srp30 = data.srp30;
-        const srp45 = data.srp45;
-        const srp60 = data.srp60;
-
-        const trace30 = {
-            x: srp30,
-            type: 'histogram',
-            name: '30M',
-            opacity: 0.5,
-            marker: { color: 'blue' }
-        };
-
-        const trace45 = {
-            x: srp45,
-            type: 'histogram',
-            name: '45M',
-            opacity: 0.5,
-            marker: { color: 'green' }
-        };
-
-        const trace60 = {
-            x: srp60,
-            type: 'histogram',
-            name: '60M',
-            opacity: 0.5,
-            marker: { color: 'red' }
-        };
-
-        const layout = {
-            title: 'Distribution of Surprises',
-            barmode: 'overlay',
-            xaxis: { title: 'Surprise' },
-            yaxis: { title: 'Count' }
-        };
-
-        Plotly.newPlot(chartId, [trace30, trace45, trace60], layout);
-    }
-    */
 });
